0172ef9b60b31deee2657be7b1a722d21601a613
Roman Boiko <rboi.personal@gmail.com>
2024-02-29 18:48:06 +0200

    added @axe-core/react

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axe from '@axe-core/react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
+
+if (process.env.NODE_ENV !== 'production') {
+  axe(React, ReactDOM, 1000);
+}
+
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
